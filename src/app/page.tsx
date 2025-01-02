009c45f66f393466ad52242249c7aa14a658b5df
Dean Johnson <dean.johnson@howdens.com>
2025-01-02 16:46:38 +0000

    added env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
+  const value = process.env.HELLO ?? "boo, didn't work!";
+
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -48,6 +51,9 @@ export default function Home() {
           >
             Read our docs
           </a>
+        </div>
+        <div>
+          <h1>{value}</h1>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
